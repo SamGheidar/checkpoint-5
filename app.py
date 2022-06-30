e428589931db0788b304ae2b86a9e4e5656103f5
SamGheidar <sam.gheidar@gmail.com>
2022-06-30 14:28:16 +0200

    BASE

--- app.py
+++ app.py
@@ -11,10 +11,21 @@ def read_phonelist(C):
 def read_phone(C, name):
     cur = C.cursor()
     print(f"SELECT phone FROM phonelist WHERE name = '{name}';")
+    print(len(name))
     cur.execute(f"SELECT phone FROM phonelist WHERE name = '{name}';")
     rows = cur.fetchall()
     cur.close()
     return rows
+
+def read_name(C, phone):
+    cur = C.cursor()
+    print(f"SELECT name FROM phonelist WHERE phone = '{phone}';")
+    print(len(phone))
+    cur.execute(f"SELECT name FROM phonelist WHERE phone = '{phone}';")
+    rows = cur.fetchall()
+    cur.close()
+    return rows
+
 def add_phone(C, name, phone):
     cur = C.cursor()
     cur.execute(f"INSERT INTO phonelist VALUES ('{name}', '{phone}');")
@@ -78,6 +89,14 @@ def api_func():
         if len(phone) < 1:
             return "not found"
         return phone[0][0]
+        
+    elif action == 'name':
+        phone = args.get('phone', default="No phone", type=str)
+        if phone == "No phone":
+            return render_template('api_usage.html', action=action)
+        name = read_name(conn, phone)
+        if len(name) < 1:
+            return "not found"
+        return name[0][0]
     else:
         return f"Unknown action: '{action}'"
-
